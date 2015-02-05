059846c3ceb1d7e0ed33b3250d23e277db0ca71e
Nick Lathe <nicklathe@icloud.com>
2015-02-04 17:44:24 -0800

    Ready for homework submition

--- js/app.js
+++ js/app.js
@@ -1,3 +1,99 @@
-/* put js code here */
+var redditApp = angular.module('RedditApp',['ui.bootstrap']);
 
-console.log('this file is running.');
+redditApp.run(function(){
+    console.log('Running...');
+});
+
+redditApp.controller('RedditController', ['$scope','$http','$modal', function($scope, $http, $modal){
+    $scope.searchTerm = '';
+    $scope.posts = {};
+
+    try {
+        $scope.terms = JSON.parse(window.localStorage.terms) || [];
+    } catch(e) {
+        console.log('error', e);
+        $scope.terms = [];
+    }
+
+    $scope.search = function(){
+
+        var req = {
+            url: 'http://www.reddit.com/search.json',
+            params: {
+                q: $scope.searchTerm
+            }
+        };
+
+        $http(req).success(function(data){
+            if($scope.terms.indexOf($scope.searchTerm) === -1){
+                $scope.terms.unshift($scope.searchTerm);
+                window.localStorage.terms = JSON.stringify($scope.terms);
+            }
+            $scope.posts = data.data.children;
+        });
+    };
+
+    $scope.reSearch = function(idx){
+
+        var req = {
+            url: 'http://www.reddit.com/search.json',
+            params: {
+                q: $scope.terms[idx]
+            }
+        };
+
+        $scope.searchTerm = $scope.terms[idx];
+
+        $http(req).success(function(data){
+            $scope.posts = data.data.children;
+        });
+    };
+
+    $scope.delete = function(idx){
+        $scope.terms.splice(idx, 1);
+        window.localStorage.terms = JSON.stringify($scope.terms);
+
+    };
+
+    $scope.selected = function(idx){
+        if($scope.terms[idx] == $scope.searchTerm){
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    $scope.hasPhoto = function(item) {
+        if (item.data.thumbnail.length > 10 ) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+// Modal below
+    $scope.showAbout = function(){
+        var aboutModal = $modal.open({
+            templateUrl:'aboutModal.html',
+            controller: 'AboutModalCtrl',
+            size: "md"
+        });
+    };
+
+}]);
+
+// Modal controller
+redditApp.controller('AboutModalCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance){
+    $scope.greeting = "How are you? This is an about page"
+    $scope.closeModal = function(){
+        $modalInstance.close();
+    };
+
+}]);
+
+
+
+
+
+
+
